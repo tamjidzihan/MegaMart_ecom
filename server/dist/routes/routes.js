e079b06eaddd4fad33379d2c7d39f63e3e171910
tamjidzihan <tamjidzihan@gmail.com>
2025-02-12 15:15:01 +0600

    Server test

--- server/dist/routes/routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-router.get("/test", (req, res) => {
-    res.json({ message: "API is working!" });
-});
-exports.default = router;
-//# sourceMappingURL=routes.js.map
--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -5,5 +5,47 @@ const router = Router();
 router.get("/test", (req, res) => {
     res.json({ message: "API is working!" });
 });
+router.get("/users", (req, res) => {
+    res.json({
+        id: 455514,
+        name: "Tamjid islam",
+        address: " Dhaka , Bangladesh",
+        createat: "2012-04-23T18:25:43.511Z"
+    });
+});
+router.get("/products", (req, res) => {
+    res.json([
+        {
+            id: "448add8844555",
+            title: "Iphone",
+            category: "Electronics"
+        },
+        {
+            id: "448add8844555",
+            title: "Iphone",
+            category: "Electronics"
+        },
+        {
+            id: "448add8844555",
+            title: "Iphone",
+            category: "Electronics"
+        },
+        {
+            id: "448add8844555",
+            title: "Iphone",
+            category: "Electronics"
+        },
+        {
+            id: "448add8844555",
+            title: "Iphone",
+            category: "Electronics"
+        },
+        {
+            id: "448add8844555",
+            title: "Iphone",
+            category: "Electronics"
+        },
+    ]);
+});
 
 export default router;
